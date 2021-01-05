ff8d51674715443fe717edf2e18740e19df76966
Guy Shoham <guyshoham28@gmail.com>
2021-01-05 14:40:22 +0200

    add leagues

--- scripts/leagues.py
+++ scripts/leagues.py
@@ -1,9 +1,9 @@
+import json
+
 import firebase_admin
 import requests
 from firebase_admin import credentials
 from firebase_admin import firestore
-
-import json
 
 
 def connect_db():
@@ -39,25 +39,22 @@ def fetch_data_from_api(url):
 db = connect_db()
 
 ############################ FETCH DATA USING API OR JSON FILE #########################################################
-obj = fetch_data_from_file(file_name='../json/teams_venues_2020_pl.json')
-# obj = fetch_data_from_api(url='https://v3.football.api-sports.io/teams?league=39&season=2020')
+obj = fetch_data_from_file(file_name='../json/leagues_pd.json')
+# obj = fetch_data_from_api(url='https://v3.football.api-sports.io/leagues?id=39')
 
 
 ############################ FROM HERE IS DIFFERENT PER SCRIPT #########################################################
 response = obj['response']
 
 for json_obj in response:
-    team = json_obj["team"]
-    team_name = team['name']
-    print(team_name)
+    league = json_obj["league"]
+    country = json_obj["country"]
+    seasons = json_obj["seasons"]
+    league_name = league['name']
+    print(league_name)
 
     # create document from team
-    doc_ref = db.collection(u'teams').document(f'{team_name}')
-    doc_ref.set(team)
-
-    venue = json_obj["venue"]
-    venue_name = venue['name']
-
-    # create document from venue
-    doc_ref = db.collection(u'venues').document(f'{venue_name}')
-    doc_ref.set(venue)
+    doc_ref = db.collection(u'leagues').document(f'{league_name}')
+    doc_ref.set({f'league': league}, merge=True)
+    doc_ref.set({f'country': country}, merge=True)
+    doc_ref.set({f'seasons': seasons}, merge=True)
