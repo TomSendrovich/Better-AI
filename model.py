2fdc6249f10a6c2c6683b914f78f7f3ee2306714
Guy Shoham <guyshoham28@gmail.com>
2021-05-01 19:00:52 +0300

    drop team names + option to save and load model

--- model.py
+++ model.py
@@ -1,29 +1,50 @@
-import pandas as pd
+import pickle
+
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import confusion_matrix
+import pandas as pd
 from sklearn.metrics import accuracy_score
-from sklearn.utils.validation import check_is_fitted
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
 
 
 X = pd.read_csv("combined.csv")
 Y = X['W']
 
-X = pd.concat([X,pd.get_dummies(X['HT'],prefix='HomeTeam')],axis=1)
-X = pd.concat([X,pd.get_dummies(X['AT'],prefix='AwayTeam')],axis=1)
-X = X.drop(columns=['HT','AT','W'])
+X = X.drop(columns=['HT', 'AT', 'W'])
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
 lr = MLPClassifier(max_iter=7)
-#lr = LogisticRegression(solver='liblinear',max_iter=10)
+# lr = LogisticRegression(solver='liblinear',max_iter=10)
 lr.fit(X_train, y_train)
 
+# plt.plot(lr.loss_curve_)
+# plt.show()
+
 y_hat_test = lr.predict(X_test)
-accuracy = pd.DataFrame({'Actual:':y_test,'Predicted:':y_hat_test})
-score = accuracy_score(y_test,y_hat_test)
-conf = confusion_matrix(y_test,y_hat_test)
+accuracy = pd.DataFrame({'Actual:': y_test, 'Predicted:': y_hat_test})
+score = accuracy_score(y_test, y_hat_test)
+conf = confusion_matrix(y_test, y_hat_test)
 print(score)
 print(conf)
+
+'''predict new cases'''
+X = pd.read_csv("test.csv")
+Y = X['W']
+X = X.drop(columns=['HT', 'AT', 'W'])
+y_hat_test = lr.predict(X)
+
+print(y_hat_test)
+
+# print probabilites for each class
+print(lr.predict_proba(X))
+
+'''option to save model vectors. we can load this file later and predict with it'''
+# filename = 'finalized_model.sav'
+# pickle.dump(lr, open(filename, 'wb'))
+#
+# loaded_model = pickle.load(open(filename, 'rb'))
+
+# result = loaded_model.predict_proba(X)
+# print(result)
